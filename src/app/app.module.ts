aa72f9015ec316e5ce2373b363a9f552c75293c7
s46382 <thomas.woxberg@seb.se>
2017-05-16 17:04:11 +0200

    added sliders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NouisliderModule } from 'ng2-nouislider';
 
 import { AppComponent } from './app.component';
 import { LendingInputComponent } from './lending-input/lending-input.component';
--- src/app/lending-input/lending-input.component.ts
+++ src/app/lending-input/lending-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NouisliderModule } from 'ng2-nouislider';
 
 @Component({
   selector: 'app-lending-input',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lending-input.component.css']
 })
 export class LendingInputComponent implements OnInit {
-
+interest:Number; 
   constructor() { }
 
   ngOnInit() {
   }
 
+  changeInterest(value : number) {
+    this.interest = value; 
+  }
 }
