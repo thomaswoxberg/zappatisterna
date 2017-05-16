ec04bed7ebc72d2eabbc3b4041e81c4212d72cea
s46382 <thomas.woxberg@seb.se>
2017-05-16 21:48:45 +0200

    dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { LendingInputComponent } from './lending-input/lending-input.component';
 import {ListLoansComponent} from './list-loans/list-loans.component'
 import {ApplicationService} from './services/application.service'
+import {DialogModule} from 'primeng/primeng';
+import {ConfirmDialogModule,ConfirmationService} from 'primeng/primeng';
 
 const appRoutes: Routes = [
   { path: 'lender', component: LendingInputComponent },
@@ -25,9 +28,12 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule, 
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes), 
+    DialogModule, 
+    ConfirmDialogModule,
+    BrowserAnimationsModule
   ],
-  providers: [ApplicationService],
+  providers: [ApplicationService, ConfirmationService],
   bootstrap: [AppComponent] 
 })
 export class AppModule { } 
