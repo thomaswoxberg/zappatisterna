7fc7c531f97abe2bf494d66c42c6fe852a952915
s46382 <thomas.woxberg@seb.se>
2017-05-16 18:12:35 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { NouisliderModule } from 'ng2-nouislider';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LendingInputComponent } from './lending-input/lending-input.component';
+import {ListLoansComponent} from './list-loans/list-loans.component'
+
+const appRoutes: Routes = [
+  { path: 'lender', component: LendingInputComponent },
+  { path: 'borrower', component: ListLoansComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LendingInputComponent
+    LendingInputComponent, 
+    ListLoansComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule, 
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent] 
